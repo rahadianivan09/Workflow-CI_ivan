898b8b6fbb5988a2cfadc1ea7d8cc42a3bdd7663
rahadianivan09 <ivanrahadian09@gmail.com>
2026-05-09 16:19:34 +0700

    Fix data path

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,7 +17,8 @@ from sklearn.metrics import (
 # =========================
 # CONFIG
 # =========================
-DATA_PATH = "data_preprocessing.csv"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "data_preprocessing.csv")
 TARGET = "Churn"
 EXPERIMENT_NAME = "telco_churn_rf_baseline"
 
